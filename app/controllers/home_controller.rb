b61423c8705f4f2bdea7a66c769dfaf3a049770e
Roc Yu <rociiu.yu@gmail.com>
2015-02-06 16:51:17 +0800

    implement filter form select

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @crimes = Crime.filter(params)
+    load_form_select_data
+  end
+
+  private
+
+  def load_form_select_data
+    @years            = 2013..2014
+    @forces           = Force.all.unshift(OpenStruct.new(name: 'All Forces'))
+    @crime_categories = CrimeCategory.all
   end
 end
