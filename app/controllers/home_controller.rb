168d46282d6e7b7b156256b7f0fba04b2e00240f
Roc Yu <rociiu.yu@gmail.com>
2015-02-06 20:26:41 +0800

    fix default data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,10 @@ class HomeController < ApplicationController
     @years            = 2013..2014
     @forces           = Force.all.unshift(OpenStruct.new(name: 'All Forces'))
     @crime_categories = CrimeCategory.all
+
+    params[:year] ||=  '2014'
+    params[:force] ||= 'All Forces'
+    params[:crime_category] ||= 'All crime'
   end
 
   def load_line_chart_data
